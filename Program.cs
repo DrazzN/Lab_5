6da0b623a7021d20a0c9e768c41a7bfeafe6b574
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:21:48 +0545

    Moved opening and closing curly braces to their own line to improve spacing

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@ int roll3 = dice.Next(1, 7);
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
 
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
-    if ((roll1 == roll2) && (roll2 == roll3)) {
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
+{
+    if ((roll1 == roll2) && (roll2 == roll3)) 
+    {
         Console.WriteLine("You rolled triples!  +6 bonus to total!");
         total += 6; 
-    } else {
+    } 
+    else 
+    {
         Console.WriteLine("You rolled doubles!  +2 bonus to total!");
         total += 2;
     }
