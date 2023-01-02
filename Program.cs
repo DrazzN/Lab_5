edc679b82efa0f84c5431b9c1d341d5b7d345737
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:13:10 +0545

    Removed the low-level descriptive comments

--- Program.cs
+++ Program.cs
@@ -1,18 +1,15 @@
 ﻿Random random = new Random();
 string[] orderIDs = new string[5];
-// Loop through each blank orderID
+
 for (int i = 0; i < orderIDs.Length; i++)
 {
-    // Get a random value that equates to ASCII letters A through E
     int prefixValue = random.Next(65, 70);
-    // Convert the random value into a char, then a string
     string prefix = Convert.ToChar(prefixValue).ToString();
-    // Create a random number, padd with zeroes
     string suffix = random.Next(1, 1000).ToString("000");
-    // Combine the prefix and suffix together, then assign to current OrderID
+
     orderIDs[i] = prefix + suffix;
 }
-// Print out each orderID
+
 foreach (var orderID in orderIDs)
 {
     Console.WriteLine(orderID);
