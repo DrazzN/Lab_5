6955cd77aaac7e944389861016133815e6c345cd
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:10:06 +0545

    Comment out lines of code of previous exercise

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿string firstName = "Bob";
-int widgetsSold = 7;
-Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+int widgetsPurchased = 7;
+// Testing a change to the message.
+// int widgetsSold = 7;
+// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
+Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
