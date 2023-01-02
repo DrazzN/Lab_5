9f1e58ca9113868359b43aacd8940e1e37e6236a
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:21:11 +0545

    Added whitespace to improve readability

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
 ﻿Random dice = new Random();
+
 int roll1 = dice.Next(1, 7);
 int roll2 = dice.Next(1, 7);
 int roll3 = dice.Next(1, 7);
+
 int total = roll1 + roll2 + roll3;
 Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+
 if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
     if ((roll1 == roll2) && (roll2 == roll3)) {
         Console.WriteLine("You rolled triples!  +6 bonus to total!");
