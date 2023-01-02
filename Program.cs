87eb1e111a26cf2f9ba008160da36ff4cdaae9c8
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:24:27 +0545

    Start with the unreadable code

--- Program.cs
+++ Program.cs
@@ -1,22 +1,13 @@
-﻿Random dice = new Random();
-
-int roll1 = dice.Next(1, 7);
-int roll2 = dice.Next(1, 7);
-int roll3 = dice.Next(1, 7);
-
-int total = roll1 + roll2 + roll3;
-Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
-
-if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) 
-{
-    if ((roll1 == roll2) && (roll2 == roll3)) 
-    {
-        Console.WriteLine("You rolled triples!  +6 bonus to total!");
-        total += 6; 
-    } 
-    else 
-    {
-        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
-        total += 2;
-    }
-}
+﻿string str = "The quick brown fox jumps over the lazy dog.";
+// convert the message into a char array
+char[] charMessage = str.ToCharArray();
+// Reverse the chars
+Array.Reverse(charMessage);
+int x = 0;
+// count the o's
+foreach (char i in charMessage) { if (i == 'o') { x++; } }
+// convert it back to a string
+string new_message = new String(charMessage);
+// print it out
+Console.WriteLine(new_message);
+Console.WriteLine($"'o' appears {x} times.");
