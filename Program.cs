e9bb7b1737f780c4c4d4f229a27ffded3f2a2d40
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:14:09 +0545

    Added comments to explain the higher-level purpose of code

--- Program.cs
+++ Program.cs
@@ -1,4 +1,10 @@
-﻿Random random = new Random();
+﻿/*
+  The following code creates five random OrderIDs
+  to test the fraud detection process.  OrderIDs 
+  consist of a letter from A to E, and a three
+  digit number. Ex. A123.
+*/
+Random random = new Random();
 string[] orderIDs = new string[5];
 
 for (int i = 0; i < orderIDs.Length; i++)
