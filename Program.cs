32f60b1259a7f2a419583eae068722119170004e
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:10:40 +0545

    Deleted the commented code

--- Program.cs
+++ Program.cs
@@ -1,6 +1,3 @@
 ﻿string firstName = "Bob";
 int widgetsPurchased = 7;
-// Testing a change to the message.
-// int widgetsSold = 7;
-// Console.WriteLine($"{firstName} sold {widgetsSold} widgets.");
 Console.WriteLine($"{firstName} purchased {widgetsPurchased} widgets.");
