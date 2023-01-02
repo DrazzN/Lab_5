82568bae11ead8c9b61fbe97dbadd4d8bded09d3
DARKN <darkrazznakarmi@gmail.com>
2023-01-03 04:16:51 +0545

    Added code as a starting point for exercise

--- Program.cs
+++ Program.cs
@@ -1,11 +1,15 @@
-﻿// Example 1:
-Console
-.
-WriteLine
-(
-"Hello World!"
-)
-;
-
-// Example 2:
-string firstWord="Hello";string lastWord="World";Console.WriteLine(firstWord+" "+lastWord+"!");
+﻿Random dice = new Random();
+int roll1 = dice.Next(1, 7);
+int roll2 = dice.Next(1, 7);
+int roll3 = dice.Next(1, 7);
+int total = roll1 + roll2 + roll3;
+Console.WriteLine($"Dice roll: {roll1} + {roll2} + {roll3} = {total}");
+if ((roll1 == roll2) || (roll2 == roll3) || (roll1 == roll3)) {
+    if ((roll1 == roll2) && (roll2 == roll3)) {
+        Console.WriteLine("You rolled triples!  +6 bonus to total!");
+        total += 6; 
+    } else {
+        Console.WriteLine("You rolled doubles!  +2 bonus to total!");
+        total += 2;
+    }
+}
